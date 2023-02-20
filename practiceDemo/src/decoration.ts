101feb8012fc69b2a650d13188e9a00547a671a7
ZoeWann <442314874@qq.com>
2023-02-20 19:02:48 +0800

    feat:装饰器学习

--- practiceDemo/src/decoration.ts
+++ practiceDemo/src/decoration.ts
@@ -1,13 +1,34 @@
-class greet {
-    greeting: string;
-    constructor(public name: string) {
-        this.greeting = "greeting";
-    }
-
-    sayHello() {
-        console.log(`Hello,${this.name}`);
+/**
+ * 
+ * @param target 被装饰的对象
+ * @param propertyKey 被装饰的属性名称
+ * @param descriptor 该属性的描述
+ * @returns 
+ */
+function smile(target: Greeter, propertyKey: string, descriptor: PropertyDescriptor) {
+    return {
+        ...descriptor,
+        value: function (name: string) {
+            console.log('smile');
+            //调用被装饰的方法
+            return descriptor.value(name);
+        }
     }
 }
 
-const g = new greet("Alice");
-g.sayHello();
+
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+
+    @smile
+    greet(name: string): string {
+        console.log(`Welcome,${name}`);
+        return "Hello"
+    }
+}
+
+const g = new Greeter("msg");
+g.greet("Alice");
